b64f0c9afd8d62138e07d7f5521cd347c32e51fd
ALCATRAST_LEFT <leonid.efremov.99@gmail.com>
2024-01-24 23:11:44 +0300

    clien added

---
+++ ESP_Client_Arduino/glados_client/anim_labels.h
@@ -0,0 +1,26 @@
+void RunAnimation_undefined_0(){
+  Serial.println("Animation undefined ran.");
+
+}
+void RunAnimation_bitls_1(){
+  Serial.println("Animation bitls ran.");
+
+}
+void RunAnimation_show_2(){
+  Serial.println("Animation show itself ran.");
+
+}
+
+void RunAnimation_joke_3(){
+Serial.println("Animation joke ran.");
+
+}
+
+void RunAnimation_fact_4(){
+  Serial.println("Animation fact ran.");
+
+}
+
+void RunAnimation_dance_5(){
+
+}
